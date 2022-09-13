c93ba578d947297ffe521ff331b2da341b82aee3
huynguyentel <huy.nguyen@tel.com>
2022-09-13 09:30:08 +0700

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    # FRESHMAN = 'FR'
+    # SOPHOMORE = 'SO'
+    # JUNIOR = 'JR'
+    # SENIOR = 'SR'
+    # GRADUATE = 'GR'
+    # YEAR_IN_SCHOOL_CHOICES = [
+    #     (FRESHMAN, 'Freshman'),
+    #     (SOPHOMORE, 'Sophomore'),
+    #     (JUNIOR, 'Junior'),
+    #     (SENIOR, 'Senior'),
+    #     (GRADUATE, 'Graduate'),
+    # ]
+    author = models.ForeignKey(
+        'auth.User',
+        # choices=YEAR_IN_SCHOOL_CHOICES,
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
